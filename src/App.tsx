68b123d76463ecbd5583ab729a24a7ef928ed314
ioane-tech <ioturman4@gmail.com>
2024-11-30 09:18:32 +0400

    fixed repo problem

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Supernova technical task</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
